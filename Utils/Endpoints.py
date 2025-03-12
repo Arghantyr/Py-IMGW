6f4686b81af2a67a3a6c549b519606a47d1ee67d
Jeczybula <kakoori@gmail.com>
2025-03-12 12:16:20 +0100

    feat: Add hydro/hydro2 endpoints
    Add hydro and hydro2 endpoints

--- Utils/Endpoints.py
+++ Utils/Endpoints.py
@@ -2,7 +2,10 @@ from requests import get
 
 SYNOP_GENERAL_URL='https://danepubliczne.imgw.pl/api/data/synop'
 METEO_GENERAL_URL='https://danepubliczne.imgw.pl/api/data/meteo'
+HYDRO_GENERAL_URL='https://danepubliczne.imgw.pl/api/data/hydro'
+HYDRO2_GENERAL_URL='https://danepubliczne.imgw.pl/api/data/hydro2'
 DATA_OPTIONS=['json', 'xml', 'csv', 'html']
+HYDRO_GROUPS=['hydro', 'hydro2']
 
 class Synop:
     def __init__(self):
@@ -60,6 +63,25 @@ class Meteo:
         try:
             assert data_format in DATA_OPTIONS
             result = get(f"{METEO_GENERAL_URL}/format/{data_format}")
+            return result._content, result.status_code
+        except AssertionError:
+            raise Exception("Invalid input")
+        except Exception as e:
+            raise Exception(f"{e}")
+
+class Hydro:
+    def __init__(self):
+        pass
+
+    def get_all_stations(self,
+                         station_group:str='hydro'):
+        try:
+            assert station_group in HYDRO_GROUPS
+            if station_group == 'hydro':
+                result = get(HYDRO_GENERAL_URL)
+            elif station_group == 'hydro2':
+                result = get(HYDRO2_GENERAL_URL)
+
             return result._content, result.status_code
         except AssertionError:
             raise Exception("Invalid input")
